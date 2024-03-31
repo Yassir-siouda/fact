8325215ec3f828f550e5dc403d1775a7c180386f
Yassir-siouda <72074295+Yassir-siouda@users.noreply.github.com>
2024-04-01 00:13:35 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Importer Navigate
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './compenent/Connexions/connexion';
 import Inscriptions from './compenent/Inscriptions/Inscriptions';
 import Menu from './compenent/Menu/Menu';
 import Profile from './compenent/Profile/Profile';
-
 import Facture from './compenent/Facture/Facture';
 import FactureAffichage from './compenent/FactureAffichage/FactureAffichage';
 
@@ -17,9 +16,7 @@ function App() {
         <Route path="/" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/inscriptions" element={<Inscriptions />} />
         <Route path="/menu" element={isLoggedIn ? <Menu /> : <Navigate to="/" />} />
-      
         <Route path="/profile" element={<Profile />} />
-           <Route path="/profile" element={<Profile />} />
         <Route path="/facture" element={<Facture />} />
         <Route path="/facture-affichage" element={<FactureAffichage />} />
       </Routes>
